e77bafd900231f1beaf77cf6238b7e90c86c1d1f
breakfastattwelve <l3ooksharkel2@gmail.com>
2025-09-22 15:57:47 +0700

    finished

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import bodyParser from "body-parser";
 
 // 2. Create an express app and set the port number.
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const API_URL = "https://secrets-api.appbrewery.com/random";
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,13 +14,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // 3. Use the public folder for static files.
 app.use(express.static("public"));
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
 // 4. When the user goes to the home page it should render the index.ejs file.
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get(API_URL);
     const result = response.data;
     const jsonData = JSON.stringify(result);
-    console.log(result);
     res.render("index.ejs", {
       secret: result.secret,
       user: result.username,
@@ -37,3 +39,6 @@ app.get("/", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// Export the Express app for Vercel
+export default app;
